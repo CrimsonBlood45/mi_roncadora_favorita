5368701b89643d1b2b4ada1ad246d06d2b397e7e
CrimsonBlood45 <159664058+CrimsonBlood45@users.noreply.github.com>
2024-02-10 19:52:41 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,19 @@
+var textos = ["Por queeeeeeeeee :(", "Pero va a haber sushi :/", "Te prometo que no te vas a decepcionar", "Porfavor di q sí :(", "Te compro chocolateeeees"]; // Lista de textos
+function cambiarTexto() {
+    var texto = document.getElementById("texto");
+    var indiceAleatorio = Math.floor(Math.random() * textos.length); // Genera un índice aleatorio
+    texto.innerHTML = textos[indiceAleatorio]; // Selecciona un texto aleatorio y lo muestra
+    var botonNo = document.getElementById("botonNo");
+    var nuevaPosicionX = Math.floor(Math.random() * window.innerWidth);
+    var nuevaPosicionY = Math.floor(Math.random() * window.innerHeight);
+    botonNo.style.left = nuevaPosicionX + "px";
+    botonNo.style.top = nuevaPosicionY + "px";
+}
+function cambiarPagina() {
+    // Obtener el elemento de texto por su ID
+    var texto = document.getElementById("texto");
+    
+    // Cambiar el contenido del texto
+    texto.innerHTML = "Es una cita entoncesssssssssssss :)))";
+    window.open("https://www.youtube.com/watch?v=NWT9sVXYgzc", "_blank");
+}
